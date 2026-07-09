4bb2e9fa734adfec56c52f0b49872d12f0f54ddc
jiujiu-dot <juliapiasecka@email.com>
2026-07-09 18:29:16 +0200

    fixing scrolling

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-
-  const frame = document.querySelector('.scroll-frame-image');
-  if (frame) {
-    const scrollTop = window.scrollY;
-    const revealPoint = 5000;
-    frame.style.opacity = scrollTop > revealPoint ? '1' : '0';
-  }
 });
