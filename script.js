c6f0b836a3ea7209d6fbc7a9e70e63850f0e6b6e
jiujiu-dot <juliapiasecka@email.com>
2026-07-26 21:19:53 +0200

    added layout

--- script.js
+++ script.js
@@ -24,10 +24,7 @@ document.querySelectorAll('.frame-extra[data-target-image]').forEach((tile) => {
   tile.addEventListener('click', () => {
     const gallery = document.querySelector('.bottom-gallery');
 
-    if (tile.classList.contains('one')) {
-      gallery?.classList.remove('is-hidden');
-    }
-
+    gallery?.classList.remove('is-hidden');
     swapBottomImage(tile.dataset.targetImage);
     gallery?.scrollIntoView({ behavior: 'smooth', block: 'start' });
   });
